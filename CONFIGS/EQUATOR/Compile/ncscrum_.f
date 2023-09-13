82ed84dcb0a5855c7fa907dc7e955aea3dae1f23
NkululekoMemela <nkululekolerato@gmail.com>
2023-09-13 10:59:17 +0200

    Clean-up the repo

---
+++ CONFIGS/EQUATOR/Compile/ncscrum_.f
@@ -0,0 +1,140 @@
+      module ncscrum
+      use param
+      integer*4 filetype_his, filetype_avg
+     &       ,filetype_dia, filetype_dia_avg
+     &       ,filetype_diaM, filetype_diaM_avg
+     &       ,filetype_diags_vrt, filetype_diags_vrt_avg
+     &       ,filetype_diags_ek, filetype_diags_ek_avg
+     &       ,filetype_diags_pv, filetype_diags_pv_avg
+     &       ,filetype_diags_eddy_avg
+     &       ,filetype_surf, filetype_surf_avg
+     &       ,filetype_diabio, filetype_diabio_avg
+      parameter (filetype_his=1, filetype_avg=2,
+     &           filetype_dia=3, filetype_dia_avg=4,
+     &           filetype_diaM=5, filetype_diaM_avg=6,
+     &           filetype_diags_vrt=7, filetype_diags_vrt_avg=8,
+     &           filetype_diags_ek=9, filetype_diags_ek_avg=10,
+     &           filetype_diags_pv=11, filetype_diags_pv_avg=12,
+     &           filetype_diags_eddy_avg=17,
+     &           filetype_surf=13, filetype_surf_avg=14,
+     &           filetype_diabio=15,filetype_diabio_avg=16)
+      integer*4 iloop, indextemp
+      integer*4 indxTime, indxZ, indxUb, indxVb
+      parameter (indxTime=1, indxZ=2, indxUb=3, indxVb=4)
+      integer*4 indxU, indxV
+      parameter (indxU=6, indxV=7)
+      integer*4 indxT
+      parameter (indxT=indxV+1)
+      integer*4 indxBSD, indxBSS
+      parameter (indxBSD=indxV+ntrc_temp+ntrc_salt+ntrc_pas+ntrc_bio+1,
+     &           indxBSS=101)
+      integer*4 indxO, indxW, indxR, indxVisc, indxDiff, indxAkv, 
+     &                                                           indxAkt
+      parameter (indxO=indxV+ntrc_temp+ntrc_salt+ntrc_pas+ntrc_bio
+     &                      +ntrc_sed+ntrc_substot
+     &           +ntrc_diats+ntrc_diauv+ntrc_diavrt+ntrc_diaek
+     &           +ntrc_diapv+ntrc_diaeddy+ntrc_surf+ntrc_diabio+1,
+     &           indxW=indxO+1, indxR=indxO+2, indxVisc=indxO+3,
+     &           indxDiff=indxO+4,indxAkv=indxO+5, indxAkt=indxO+6)
+      integer*4 indxSSH
+      parameter (indxSSH=indxAkv+ntrc_temp+12)
+      integer*4 indxbvf
+      parameter (indxbvf=indxSSH+1)
+      integer*4 indxSUSTR, indxSVSTR
+      parameter (indxSUSTR=indxSSH+2, indxSVSTR=indxSSH+3)
+      integer*4 indxTime2
+      parameter (indxTime2=indxSSH+4)
+      integer*4 indxShflx, indxShflx_rsw
+      parameter (indxShflx=indxSUSTR+5)
+      parameter (indxShflx_rsw=indxShflx+1)
+      integer*4 indxSST, indxdQdSST
+      parameter (indxSST=indxShflx_rsw+1, indxdQdSST=indxShflx_rsw+2)
+      integer*4 indxWstr
+      parameter (indxWstr=indxSUSTR+23)
+      integer*4 indxUWstr
+      parameter (indxUWstr=indxSUSTR+24)
+      integer*4 indxVWstr
+      parameter (indxVWstr=indxSUSTR+25)
+      integer*4 indxBostr
+      parameter (indxBostr=indxSUSTR+26)
+      integer*4 indxBustr, indxBvstr
+      parameter (indxBustr=indxSUSTR+27,  indxBvstr=indxBustr+1)
+      integer*4 indxWWA,indxWWD,indxWWP,indxWEB,indxWED,indxWER
+      parameter (indxWWA=indxSUSTR+42, indxWWD=indxWWA+1,
+     &           indxWWP=indxWWA+2
+     &                             )
+      integer*4 r2dvar, u2dvar, v2dvar, p2dvar, r3dvar,
+     &                u3dvar, v3dvar, p3dvar, w3dvar,
+     &                pw3dvar, b3dvar
+      parameter (r2dvar=0, u2dvar=1, v2dvar=2, p2dvar=3,
+     & r3dvar=4, u3dvar=5, v3dvar=6, p3dvar=7, w3dvar=8,
+     & pw3dvar=11, b3dvar=12)
+      integer*4 xi_rho,xi_u, eta_rho,eta_v
+      parameter (xi_rho=LLm+2,  xi_u=xi_rho-1,
+     &           eta_rho=MMm+2, eta_v=eta_rho-1)
+      integer*4 ncidfrc, ncidbulk, ncidclm,  ntsms
+     &     , ncidqbar, ncidbtf
+     &     , ntsrf,  ntssh,  ntsst, ntsss, ntuclm
+     &     , ntbulk, ntqbar, ntww
+      integer*4 nttclm(NT), ntstf(NT), nttsrc(NT)
+     &       , ntbtf(NT)
+      integer*4 ncidrst, nrecrst,  nrpfrst
+     &      , rstTime, rstTime2, rstTstep, rstZ,    rstUb,  rstVb
+     &                         , rstU,    rstV
+      integer*4 rstT(NT)
+      integer*4  ncidhis, nrechis,  nrpfhis
+     &      , hisTime, hisTime2, hisTstep, hisZ,    hisUb,  hisVb
+     &      , hisBostr, hisWstr, hisUWstr, hisVWstr
+     &      , hisBustr, hisBvstr
+     &      , hisShflx, hisSwflx, hisShflx_rsw, hisBhflx, hisBwflx
+     &      , hisU,   hisV,   hisR,    hisHbl, hisHbbl
+     &      , hisO,   hisW,   hisVisc, hisDiff
+     &      , hisAkv, hisAkt, hisAks
+     &      , hisbvf
+      integer*4 hisT(NT)
+      logical wrthis(500+NT)
+      common/incscrum/
+     &     ncidfrc, ncidbulk,ncidclm, ncidqbar, ncidbtf
+     &     , ntsms, ntsrf, ntssh, ntsst
+     &     , ntuclm, ntsss, ntbulk, ntqbar, ntww
+     &     ,  nttclm, ntstf, nttsrc, ntbtf
+     &      , ncidrst, nrecrst,  nrpfrst
+     &      , rstTime, rstTime2, rstTstep, rstZ,    rstUb,  rstVb
+     &                         , rstU,    rstV
+     & ,   rstT
+     &      , ncidhis, nrechis,  nrpfhis
+     &      , hisTime, hisTime2, hisTstep, hisZ,    hisUb,  hisVb
+     &      , hisBostr, hisWstr, hisUWstr, hisVWstr
+     &      , hisBustr, hisBvstr
+     &      , hisShflx, hisSwflx, hisShflx_rsw
+     &      , hisBhflx, hisBwflx
+     &      , hisU,    hisV,     hisT,    hisR
+     &      , hisO,    hisW,     hisVisc, hisDiff
+     &      , hisAkv,  hisAkt,   hisAks
+     &      , hisHbl,  hisHbbl
+     &      , hisbvf
+     &      , wrthis
+      character*80 date_str, title, start_date
+      character*80 origin_date, start_date_run
+      integer*4      start_day, start_month, start_year
+     &         ,   start_hour, start_minute, start_second
+     &         ,   origin_day, origin_month, origin_year
+     &         ,   origin_hour, origin_minute, origin_second
+      REAL(kind=8)             :: origin_date_in_sec
+      character*180 ininame,  grdname,  hisname
+     &         ,   rstname,  frcname,  bulkname,  usrname
+     &         ,   qbarname, tsrcname
+     &         ,   btfname
+      character*75  vname(20, 500)
+      common /cncscrum/   date_str,   title,  start_date
+     &         ,   origin_date, start_date_run
+     &         ,   ininame,  grdname, hisname
+     &         ,   rstname,  frcname, bulkname,  usrname
+     &         ,   qbarname, tsrcname
+     &         ,   btfname, origin_date_in_sec
+     &         ,   start_day, start_month, start_year
+     &         ,   start_hour, start_minute, start_second
+     &         ,   origin_day, origin_month, origin_year
+     &         ,   origin_hour, origin_minute, origin_second
+     &                                ,   vname
+      end module ncscrum
---
+++ CONFIGS/EQUATOR/Compile/setup_kwds_.f
@@ -0,0 +1,140 @@
+      subroutine setup_kwds (ierr)
+      implicit none
+      integer*4 ierr, is,ie
+      integer*4  LLm,Lm,MMm,Mm,N, LLm0,MMm0
+      parameter (LLm0=40,   MMm0=32,   N=32)
+      parameter (LLm=LLm0,  MMm=MMm0)
+      integer*4 Lmmpi,Mmmpi,iminmpi,imaxmpi,jminmpi,jmaxmpi
+      common /comm_setup_mpi1/ Lmmpi,Mmmpi
+      common /comm_setup_mpi2/ iminmpi,imaxmpi,jminmpi,jmaxmpi
+      integer*4 NSUB_X, NSUB_E, NPP
+      parameter (NPP=1)
+      parameter (NSUB_X=1, NSUB_E=NPP)
+      integer*4 NWEIGHT
+      parameter (NWEIGHT=1000)
+      integer*4 stdout, Np, padd_X,padd_E
+      parameter (stdout=6)
+      parameter (Np=N+1)
+      parameter (Lm=LLm, Mm=MMm)
+      parameter (padd_X=(Lm+2)/2-(Lm+1)/2)
+      parameter (padd_E=(Mm+2)/2-(Mm+1)/2)
+      integer*4 NSA, N2d,N3d, size_XI,size_ETA
+      integer*4 se,sse, sz,ssz
+      parameter (NSA=28)
+      parameter (size_XI=7+(Lm+NSUB_X-1)/NSUB_X)
+      parameter (size_ETA=7+(Mm+NSUB_E-1)/NSUB_E)
+      parameter (sse=size_ETA/Np, ssz=Np/size_ETA)
+      parameter (se=sse/(sse+ssz), sz=1-se)
+      parameter (N2d=size_XI*(se*size_ETA+sz*Np))
+      parameter (N3d=size_XI*size_ETA*Np)
+      real Vtransform
+      parameter (Vtransform=1)
+      integer*4   NT, NTA, itemp, NTot
+      integer*4   ntrc_temp, ntrc_salt, ntrc_pas, ntrc_bio, ntrc_sed
+      integer*4   ntrc_subs, ntrc_substot
+      parameter (itemp=1)
+      parameter (ntrc_temp=1)
+      parameter (ntrc_salt=0)
+      parameter (ntrc_pas=0)
+      parameter (ntrc_bio=0)
+      parameter (ntrc_subs=0, ntrc_substot=0)
+      parameter (ntrc_sed=0)
+      parameter (NTA=itemp+ntrc_salt)
+      parameter (NT=itemp+ntrc_salt+ntrc_pas+ntrc_bio+ntrc_sed)
+      parameter (NTot=NT)
+      integer*4   ntrc_diats, ntrc_diauv, ntrc_diabio
+      integer*4   ntrc_diavrt, ntrc_diaek, ntrc_diapv
+      integer*4   ntrc_diaeddy, ntrc_surf
+      parameter (ntrc_diabio=0)
+      parameter (ntrc_diats=0)
+      parameter (ntrc_diauv=0)
+      parameter (ntrc_diavrt=0)
+      parameter (ntrc_diaek=0)
+      parameter (ntrc_diapv=0)
+      parameter (ntrc_diaeddy=0)
+      parameter (ntrc_surf=0)
+      integer*4 max_opt_size
+      parameter (max_opt_size=4400)
+      character*4400 Coptions,srcs
+      common /strings/ Coptions,srcs
+      do is=1,max_opt_size
+        Coptions(is:is)=' '
+      enddo
+      is=1
+      ie=is + 5
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='title'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +13
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='time_stepping'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 7
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='S-coord'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 7
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='initial'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 7
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='restart'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 7
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='history'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +22
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='primary_history_fields'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +24
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='auxiliary_history_fields'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 4
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='rho0'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +12
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='lateral_visc'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +11
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='bottom_drag'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is + 6
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='gamma2'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +12
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='tracer_diff2'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      ie=is +11
+      if (ie.ge.max_opt_size) goto 99
+      Coptions(is:ie)='lin_EOS_cff'
+      Coptions(ie+1:ie+1)=' '
+      is=ie+2
+      return
+  99   write(stdout,'(/1x,A,A/14x,A)')
+     &  'SETUP_KWDS ERROR: Unsufficient size of string Coptions',
+     &  'in file "strings.h".', 'Increase the size it and recompile.'
+      ierr=ierr+1
+      return
+      end
